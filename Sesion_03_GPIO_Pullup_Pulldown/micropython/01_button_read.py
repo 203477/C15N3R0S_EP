7612ef641296c78012a622be15f3d570bbf5e5c3
C15N3R0S <203477@iberopuebla.mx>
2026-09-10 00:58:20 -0600

    update
    Implement button reading with pull-up resistor on GP16.

--- Sesion_03_GPIO_Pullup_Pulldown/micropython/01_button_read.py
+++ Sesion_03_GPIO_Pullup_Pulldown/micropython/01_button_read.py
@@ -1,1 +1,14 @@
+from machine import Pin
+from time import sleep_ms
 
+# Entrada digital en GP16 con resistencia pull-up interna
+# Reposo (abierto) = 1
+# Presionado (cerrado a GND) = 0
+button = Pin(16, Pin.IN, Pin.PULL_UP)
+
+print("Iniciando lectura de GP16 (Pull-up)...")
+
+while True:
+    estado = button.value()
+    print("button =", estado)
+    sleep_ms(200)
